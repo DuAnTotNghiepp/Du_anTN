b7c52df4b64305036a3ebfedaa0b620e8abd09a3
duph39199 <dutvph39199@fpt.edu.vn>
2024-10-15 19:00:37 +0700

    push list all SP, SP hot

---
+++ app/Http/Controllers/Client/ClientController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Catalogues;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $data = Catalogues::query()->get();
+
+    // Lấy tất cả sản phẩm
+    $listSp = Product::where('is_active', 1)->paginate(5);
+
+    // Lấy sản phẩm hot
+    $listHot = Product::where('is_hot_deal', 1)->get();
+
+    return view('client.home', compact(['listSp', 'listHot', 'data']));
+    }
+
+}
