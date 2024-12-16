a5c7590ba348f05d5d1989191d529149a9312290
Hungddph <hungddph38084@fpt.edu.vn>
2024-12-16 23:00:02 +0700

    thong ke sp va sua lai trang thai

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,22 +11,19 @@ class ProductController extends Controller
 {
     public function detail($slug)
 {
-    // Lấy sản phẩm theo slug
     $product = Product::where('slug', $slug)->firstOrFail();
     
-    // Lấy bình luận cho sản phẩm với phân trang
-    $comments = BinhLuan::where('product_id', $product->id)->orderBy('created_at', 'desc')->paginate(6); // Hiển thị 6 bình luận mỗi trang
+    $comments = BinhLuan::where('product_id', $product->id)->orderBy('created_at', 'desc')->paginate(6); 
 
-    // Tính toán điểm đánh giá trung bình
-    $averageRating = $comments->count() > 0 ? $comments->avg('rating') : 0; // Nếu không có bình luận thì cho trung bình là 0
+    $averageRating = $comments->count() > 0 ? $comments->avg('rating') : 0; 
 
     return view('product-detail', compact('product', 'comments', 'averageRating'));
 }
 
+
     
     public function indexWithComments()
     {
-        // Lấy danh sách sản phẩm cùng với số bình luận và điểm đánh giá trung bình hùng..
         $listPro = Product::with(['binh_luans' => function ($query) {
             $query->select('product_id', DB::raw('avg(rating) as average_rating'))
                   ->groupBy('product_id');
